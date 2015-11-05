de67f8858a7f99b3cc764db84903d48e66c2173c
yuka10440 <yuka.t310@gmail.com>
2015-11-05 19:41:30 +0900

    change color to red

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require "./lib/actor/human"
-require "./lib/decorator/character"
+# require "./lib/decorator/charactor"
+require "./lib/decorator/color_red"
 
 actor = Actor::Human.new 'My name is Yuka'
-decorator = Decorator::Character.new actor
+# decorator = Decorator::Character.new actor
+decorator = Decorator::ColorRed.new actor
 puts decorator.show_message
